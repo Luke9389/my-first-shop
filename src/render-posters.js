1d42c32cfa4c43ec122179b457c3459103e6d26b
Luke Donahue <luke9389@gmail.com>
2019-07-23 09:45:17 -0700

    created passing renderPosters function

---
+++ src/render-posters.js
@@ -0,0 +1,35 @@
+function renderPosters(jungle) {
+    const li = document.createElement('li');
+    li.className = jungle.code;
+    li.title = jungle.name + ' by ' + jungle.category;
+
+    const h3 = document.createElement('h3');
+    h3.textContent = jungle.name;
+    li.appendChild(h3);
+
+    const img = document.createElement('img');
+    img.src = jungle.image;
+    img.alt = jungle.name + ' image';
+    li.appendChild(img);
+
+    const artistP = document.createElement('p');
+    artistP.className = 'artist';
+    artistP.textContent = jungle.category;
+    li.appendChild(artistP);
+
+    const priceP = document.createElement('p');
+    priceP.className = 'price';
+
+    const usd = jungle.price.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+    priceP.textContent = usd;
+    li.appendChild(priceP);
+
+    const button = document.createElement('button');
+    button.textContent = 'Add';
+    button.value = jungle.code;
+    li.appendChild(button);
+
+    return li;
+}
+
+export default renderPosters;
--- test/render-posters.test.js
+++ test/render-posters.test.js
@@ -2,34 +2,20 @@ import renderPosters from '../src/render-posters.js';
 
 const test = QUnit.test;
 
-Qunit.module('Render Posters');
+QUnit.module('Render Posters');
 
 test('renders a fruit', assert => {
     //arrange
     const jungle = {
         code: 'AN-jungle',
         image: 'assets/AN-jungle.jpg',
-        name: 'Jungle',
+        name: 'Bone Jungle',
         description: 'Jungle by Alex Nino',
         category: 'Alex Nino',
         price: 600.00,
         cost: 10
     };
-    const expected = '<li class="apple" title="A sweet, delicious forbidden-to-some
-    treat">
-        < h3 > Bone Jungle</h3 >
-            <img src="assets/AN-jungle.jpg">
-                <p>
-                    Alex Nino
-                    </p>
-                <p>
-                    $600.00
-                    </p>
-                <p>
-                    <button value="apple">Add</button>
-                </p>
-                </li>
-
+    const expected = '<li class="AN-jungle" title="Bone Jungle by Alex Nino"><h3>Bone Jungle</h3><img src="assets/AN-jungle.jpg" alt="Bone Jungle image"><p class="artist">Alex Nino</p><p class="price">$600.00</p><button value="AN-jungle">Add</button></li>';
 
     //act
     const dom = renderPosters(jungle);
