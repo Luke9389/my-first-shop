d0c718d710ee7768fc932c7b1cfc8fd260ff6cea
Luke Donahue <luke9389@gmail.com>
2019-07-23 12:44:24 -0700

    removed whitespace

--- test/render-posters.test.js
+++ test/render-posters.test.js
@@ -5,7 +5,6 @@ const test = QUnit.test;
 QUnit.module('Render Posters');
 
 test('renders a fruit', assert => {
-    //arrange
     const jungle = {
         code: 'AN-jungle',
         image: 'assets/AN-jungle.jpg',
@@ -16,11 +15,11 @@ test('renders a fruit', assert => {
         price: 600.00,
         cost: 10
     };
+    
     const expected = '<li class="AN-jungle AN" title="Bone Jungle by Alex Nino"><h3>Bone Jungle</h3><img src="assets/AN-jungle.jpg" alt="Bone Jungle image"><p class="artist">Alex Nino</p><p class="price">$600.00</p><button value="AN-jungle">Add</button></li>';
 
-    //act
     const dom = renderPosters(jungle);
     const html = dom.outerHTML;
-    //assert
+
     assert.equal(html, expected);
 });
